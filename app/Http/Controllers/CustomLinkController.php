1288bf34c634636b9175b1760b0c69b54577ac5a
SunTStudio <mahsunmuh0@gmail.com>
2024-06-03 11:42:13 +0700

    add MCR 3 Juni 2024

---
+++ app/Http/Controllers/CustomLinkController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Custom_Link;
+use Illuminate\Http\Request;
+
+class CustomLinkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Custom_Link $custom_Link)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Custom_Link $custom_Link)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Custom_Link $custom_Link)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Custom_Link $custom_Link)
+    {
+        //
+    }
+}
