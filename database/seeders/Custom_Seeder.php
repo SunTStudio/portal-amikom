07def2d5667390b956b0d3c491f5cb2ef7ea37ec
SunStudio <mahsunmuh0@gmail.com>
2024-06-06 14:20:16 +0700

    s

--- database/seeders/Custom_Seeder.php
+++ database/seeders/Custom_Seeder.php
@@ -13,10 +13,13 @@ class Custom_Seeder extends Seeder
      */
     public function run(): void
     {
-        Custom_Link::create([
+        Custom::create([
+            'id_pengguna' => 1,
             'Nama_Link' => 'Chat GPT',
             'Source_Link' => 'https://chatgpt.com/',
             'Deskripsi' => 'Merupakan Website AI untuk membantu mahasiswa.',
+            'id_kategori_link' => '3',
+            ]);
         Custom::create([
             'id_pengguna' => 1,
             'Nama_Link' => 'Chat GPT',
